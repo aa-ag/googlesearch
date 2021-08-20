3ece14af7321a95c84c9f2806ebda08b2dc51805
aa-ag <aaron.aguerrevere@gmail.com>
2021-08-20 16:17:23 -0500

    a little redundant, but safe way to handle stuff, in case url is broken or something else happens; may refactore later on

--- words.py
+++ words.py
@@ -4,7 +4,7 @@ import requests
 
 
 ############------------ GLOBAL VARIABLE(S) ------------############
-words_list = 'http://www-personal.umich.edu/~jlawler/wordlist'
+words_list = 'http://wsonal.umich.edu/~jlawler/wordlist'
 
 ############------------ FUNCTION(S) ------------############
 def get_me_that_string():
@@ -13,9 +13,17 @@ def get_me_that_string():
      this function creates a request to the site where 
      that lives, and creates a local copy .txt file
     '''
-    request = requests.get(words_list)
+    try:
+        request = requests.get(words_list)
 
-    print(request.status_code)
+        if request.status_code != 200:
+            print("Whoops! Something went wrong...")
+            exit()
+
+        print(request.content)
+    except:
+        print("Whoops! Something went wrong...")
+
 
 
 ############------------ DRIVER CODE ------------############
