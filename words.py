d68628692f1ebfde1baab9d85facbd6b755a4cc5
aa-ag <aaron.aguerrevere@gmail.com>
2021-08-20 16:13:45 -0500

    created get_me_that_string function, getting 200 http response

--- words.py
+++ words.py
@@ -1,12 +1,23 @@
 ############------------ IMPORTS ------------############
+import bs4 
+import requests
 
 
 ############------------ GLOBAL VARIABLE(S) ------------############
-
+words_list = 'http://www-personal.umich.edu/~jlawler/wordlist'
 
 ############------------ FUNCTION(S) ------------############
+def get_me_that_string():
+    '''
+     there's only one string in `words_list`
+     this function creates a request to the site where 
+     that lives, and creates a local copy .txt file
+    '''
+    request = requests.get(words_list)
+
+    print(request.status_code)
 
 
 ############------------ DRIVER CODE ------------############
 if __name__ == "__main__":
-    pass
+    get_me_that_string()
