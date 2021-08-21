7634f46815c5f8b118dfe86bad6e5f80573d7aa1
aa-ag <aaron.aguerrevere@gmail.com>
2021-08-21 10:21:42 -0500

    removed settings.py as i could open file from root, renamed file to words_file to make shorter, and looping thru content

--- search.py
+++ search.py
@@ -1,13 +1,10 @@
 ############------------ IMPORTS ------------############
 from selenium import webdriver
-import settings
+import random
+
 
 ############------------ GLOBAL VARIABLE(S) ------------############
 url = 'https://www.google.com/'
-
-path_to_words_file = settings.path_to_words_text_file
-
-print('--->' + path_to_words_file)
 
 
 ############------------ FUNCTION(S) ------------############
@@ -17,17 +14,25 @@ def search_on():
         a random number of them and performs searches
         to confuse data-hungry search engines
        '''
-       # hardcoding a word to search on googl
-       test_word = 'hello'
+       # open words file, create a list with them, 
+       # create a range of N random indexes, slice the list
+       # using those indexes to grab and print random words
+
+       words_file = open('words_file', 'r')
+
+       content = words_file.read()
+
+       for line in content:
+              print(content)
 
        # creates a webdriver object to open the browser
        # and perform an action in there
-       driver = webdriver.Firefox()
+       # driver = webdriver.Firefox()
 
        # get google.com page and perform the search
-       driver.get(url + 'search?q=' + test_word)
+       # driver.get(url + 'search?q=' + word)
 
 
 ############------------ DRIVER CODE ------------############
-# if __name__ == "__main__":
-#     search_on()
+if __name__ == "__main__":
+    search_on()
