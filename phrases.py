d4a06fd815a26b831497ad2f4ee5cf27716589c9
aa-ag <aaron.aguerrevere@gmail.com>
2021-08-23 17:58:03 -0500

    created phrases.py to grab phrases from online sources and make searches a bit more human in search.py

---
+++ phrases.py
@@ -0,0 +1,24 @@
+############------------ DOCUMENTATION ------------############
+# https://beautiful-soup-4.readthedocs.io/en/latest/
+
+
+############------------ IMPORTS ------------############
+from bs4 import BeautifulSoup
+import requests
+
+
+############------------ GLOBAL VARIABLE(S) ------------############
+url = 'https://www.englishspeak.com/en/english-phrases?category_key=1'
+
+############------------ FUNCTION(S) ------------############
+def get_phrases():
+    '''
+     request url, bs4 elements,
+     create a local copy with all 
+     for later use
+    '''
+    pass
+
+############------------ DRIVER CODE ------------############
+if __name__ == "__main__":
+    pass
