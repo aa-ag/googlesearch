cf59678c26fa3643809892000bf643d812504d6c
aa-ag <aaron.aguerrevere@gmail.com>
2021-08-26 17:37:40 -0500

    added logic to print out spaced out version of each loop in the list of unordered lists where you can see which indexes to target for phrases

--- phrases.py
+++ phrases.py
@@ -30,10 +30,16 @@ def get_phrases():
     # print(soup.prettify())
 
     # print(soup.title)
+
     list_of_unordered_lists = soup.find_all('ul')
 
-    print(len(list_of_unordered_lists))
+    # print(len(list_of_unordered_lists))
 
+    for i, ul in enumerate(list_of_unordered_lists):
+        print('\n\n\n')
+        print(i)
+        print(ul)
+        print('\n\n\n')
 
 ############------------ DRIVER CODE ------------############
 if __name__ == "__main__":
